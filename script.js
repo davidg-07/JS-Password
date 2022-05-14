a9ad323f771304b4f7000d9406bd2ecc1c0ecb73
David Galvez <david.dmg17@gmail.com>
2022-05-13 21:58:32 -0700

    added funstion

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var generatePassword = "generated password";
+  console.log("clicket generate password");
+  return generatedPassword;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
